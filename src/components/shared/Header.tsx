60daa23b11b81a8059114c90daf99331c32ac3da
alvar.dev <alvaropedrozo07@gmail.com>
2025-04-18 00:24:34 -0500

    refactor: clean up Footer and Header components, remove unused ListItems export

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -5,11 +5,9 @@ export function Header(){
     <header>
       <nav>
         <ul>
-          <li></li>
         </ul>
       </nav>
       <Image src="/logo.svg" width={96} height={30} alt="Logotipo de Chevrolet" />
-      <div></div>
     </header>
   );
 };
